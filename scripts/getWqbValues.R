5520285b013f9a887adf1c913c8534bea8de36c8
sergi <sruizcarmona@gmail.com>
2016-10-04 17:28:38 +0200

    fixed getWqbValues.R and tested in several examples

--- scripts/getWqbValues.R
+++ scripts/getWqbValues.R
@@ -1,6 +1,5 @@
+#read all jarz.dat files
 inputFiles=list.files(pattern="jarz.dat",recursive=T)
-
-dataf=c()
 
 shiftMinimum=function(d,minValue){
 	localMin=min(d)
@@ -9,30 +8,49 @@ shiftMinimum=function(d,minValue){
 	return(d)
 }
 
+#initiate dataf for reading files
+dataf=c()
 #read all currently created jarz.dat output files to calculate current running Wqb
-
 for(file in inputFiles){
 	r=read.table(file)
 	if(length(dataf)==0) {
-		dataf=as.vector(r[,4])
-
+		prov=cbind(as.vector(r[,4]))
+		#normalize each file when reading (set to 0 minima before half of the simulation)
+		minValue=min(prov[1:2500])
+		dataf=shiftMinimum(prov,minValue)
 	}
 	else {
-		
-		dataf=cbind(dataf,r[,4])
+		prov=cbind(as.vector(r[,4]))
+		minValue=min(prov[1:2500])
+		dataf=cbind(dataf,shiftMinimum(prov,minValue))
 	}	
-
 }
-
-
-#now lets set to 0 all minima of the work curves from the SMD simulations
+#times of the simulation (first column of any jarz.dat file --> from 2.5 to 5)
 times=r[,1]
-#get the minimum value
-minValue=min(dataf)
 
-normalizedOutput=apply(dataf,2,shiftMinimum,minValue)
+#now lets get the maximum work on all SMD curves
+maxwork=NULL
+for (d in seq(dim(dataf)[2])){
+	#calculate each maximum work for all replicas
+	#maximum has to be on second half, after minimum (to avoid maximum in the beginning)
+	maxwork=c(maxwork,max(dataf[,d][2501:5000]))	
+}
+##############################################################
+#####to plot the curves for checking the results, run the following lines:
+##############################################################
+#matplot(times,dataf,type="l")
+#for (d in seq(dim(dataf)[2])){
+#	a=dataf[,d]
+#	maxa=which(a==max(a))
+#	text(times[maxa]-0.1,a[maxa],labels=a[maxa],col=d)
+#}
+##############################################################
 
-#now lets get the minimum Work on all SMD curves at a particular time -> this is out Wqb value
-
-wqb=min(normalizedOutput[which(times==4.25),])
+#get the minimum value among each replica maximum work --> wqb
+wqb=round(min(maxwork),2)
+#if wqb is lower than 0 (negative slope), set wqb to 0
+if (wqb < 0){
+	wqb=0
+}
 cat(wqb)
+cat("\n")
